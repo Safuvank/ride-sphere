9a04031a2ba22d8b9717c9a8306e2183c3126cf0
Safuvank <safuvanvkm@gmail.com>
2026-04-29 09:49:27 +0530

    update the admin route

--- frontend/src/routes/AdminRoute.jsx
+++ frontend/src/routes/AdminRoute.jsx
@@ -1,19 +1,3 @@
-// import { useContext } from "react";
-// import { Navigate } from "react-router-dom";
-// import { AuthContext } from "../Pages/Authantication/AuthContext";
-
-// function AdminRoute({ children }) {
-//   const { user, loading } = useContext(AuthContext);
-
-//   if (loading) return null;
-
-//   const isAdmin = user && user.role === "admin";
-
-//   return isAdmin ? children : <Navigate to="/login" replace />;
-// }
-
-// export default AdminRoute;
-
 
 
 import { Navigate } from "react-router-dom";
